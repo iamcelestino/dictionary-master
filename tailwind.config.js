f94625f582005c4029f2c3a121db5886047460f6
iamcelestino <trcelestino488@gmail.com>
2024-04-19 00:11:05 -0700

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          sm: '1.5rem',
+          md: '2.4rem',
+          xl: '22rem'       
+        }
+      }
     },
     plugins: [],
   }
